7a75ebc95bdd9810a5d58c7be2c3ba87e529fb04
Krystal <n01554329@humber.ca>
2023-11-28 21:26:14 -0500

    run

--- Frontend/client/src/Login.js
+++ Frontend/client/src/Login.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import Register from './Register'; 
+import Register from './Register';
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [redirectToRegister, setRedirectToRegister] = useState(false); 
+  const [redirectToRegister, setRedirectToRegister] = useState(false);
 
   const handleLogin = async () => {
     try {
@@ -18,10 +18,8 @@ const Login = ({ onLogin }) => {
       });
 
       if (response.ok) {
-        
         onLogin();
       } else {
-       
         const errorData = await response.json();
         setErrorMessage(errorData.message);
       }
@@ -31,12 +29,51 @@ const Login = ({ onLogin }) => {
     }
   };
 
-  
   const redirectToRegisterPage = () => {
     setRedirectToRegister(true);
   };
 
-  
+  const renderErrorMessage = () => errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>;
+
+  const renderLoginForm = () => (
+    <form>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      <button type="button" onClick={handleLogin}>
+        Log In
+      </button>
+
+      <br />
+      <br />
+      <text>Don't have an account?</text>
+      <button type="button" onClick={redirectToRegisterPage}>
+        Register
+      </button>
+    </form>
+  );
+
   if (redirectToRegister) {
     return <Register onRegister={() => setRedirectToRegister(false)} />;
   }
@@ -45,47 +82,8 @@ const Login = ({ onLogin }) => {
     <div className="login-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', border: '5px solid olive', borderRadius: '10px' }}>
       <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '10px', width: '300px', backgroundColor: 'olive' }}>
         <h1>Login</h1>
-
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-
-        <form>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <button type="button" onClick={handleLogin}>
-            Log In
-          </button>
-
-          <br></br>
-          <br></br>
-          <text>Don't have an account?</text>
-          <button type="button" onClick={redirectToRegisterPage}>
-            Register
-          </button>
-        </form>
-
-   
+        {renderErrorMessage()}
+        {renderLoginForm()}
       </div>
     </div>
   );
