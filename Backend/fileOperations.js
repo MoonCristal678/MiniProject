6b97781b2e94cf480f429dc607102079f2557f26
Krystal <n01554329@humber.ca>
2023-11-29 01:45:33 -0500

    help

--- Backend/fileOperations.js
+++ Backend/fileOperations.js
@@ -14,6 +14,7 @@ import flash from 'connect-flash';
 dotenv.config();
 const app = express();
 const port = 3000;
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
