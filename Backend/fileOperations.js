38bb63b17860c31664115a219c90d6e2d057ca36
Krystal <n01554329@humber.ca>
2023-11-30 11:24:08 -0500

    push

--- Backend/fileOperations.js
+++ Backend/fileOperations.js
@@ -4,7 +4,7 @@ import { MongoClient } from 'mongodb';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     async function fetchJsonData() {
       try {
-        const response = await fetch('https://backend-j7qq.onrender.com/v1/api/users'); // Update the URL as needed
+        const response = await fetch('https://backend-j7qq.onrender.com/v1/api/users');
         const data = await response.json();
         console.log(data);
         setJsonData(data);
@@ -26,29 +26,45 @@ function App() {
     fetchJsonData();
   }, []);
 
+  const handleFetch = async (url, method, body, successCallback, errorCallback) => {
+    try {
+      const response = await fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      successCallback(data);
+    } catch (error) {
+      console.error(error);
+      errorCallback(error);
+    }
+  };
+
   const handleCreateFile = async () => {
     if (!fileName || !fileContent) {
       alert('Please enter both a file name and content.');
       return;
     }
 
-    try {
-      await fetch('https://backend-j7qq.onrender.com/v1/write', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ fileName, fileContent }),
-      });
-      setCreatedFiles((prevFiles) => ({
-        ...prevFiles,
-        [fileName]: fileContent,
-      }));
-      setFileName('');
-      setFileContent('');
-    } catch (error) {
-      console.error('Error creating file:', error);
-    }
+    handleFetch(
+      'https://backend-j7qq.onrender.com/v1/write',
+      'POST',
+      { fileName, fileContent },
+      (data) => {
+        setCreatedFiles((prevFiles) => ({
+          ...prevFiles,
+          [fileName]: fileContent,
+        }));
+        setFileName('');
+        setFileContent('');
+      },
+      (error) => {
+        console.error('Error creating file:', error);
+      }
+    );
   };
 
   const handleReadFile = async () => {
@@ -56,39 +72,34 @@ function App() {
       alert('Please enter a file name.');
       return;
     }
-  
-    try {
-      const response = await fetch(`https://backend-j7qq.onrender.com/v1/read`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ fileName: readFileName }),
-      });
-      const data = await response.text();
-      setReadContent(data.replace(/<\/?[^>]+(>|$)/g, ""));
-    } catch (error) {
-      console.error('Error reading file:', error);
-    }
+
+    handleFetch(
+      'https://backend-j7qq.onrender.com/v1/read',
+      'POST',
+      { fileName: readFileName },
+      (data) => {
+        setReadContent(data.replace(/<\/?[^>]+(>|$)/g, ''));
+      },
+      (error) => {
+        console.error('Error reading file:', error);
+      }
+    );
   };
-  
-  
 
   const handleDeleteFile = async (fileName) => {
-    try {
-      await fetch(`https://backend-j7qq.onrender.com/v1/delete`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ fileName }),
-      });
-      const updatedFiles = { ...createdFiles };
-      delete updatedFiles[fileName];
-      setCreatedFiles(updatedFiles);
-    } catch (error) {
-      console.error('Error deleting file:', error);
-    }
+    handleFetch(
+      'https://backend-j7qq.onrender.com/v1/delete',
+      'POST',
+      { fileName },
+      () => {
+        const updatedFiles = { ...createdFiles };
+        delete updatedFiles[fileName];
+        setCreatedFiles(updatedFiles);
+      },
+      (error) => {
+        console.error('Error deleting file:', error);
+      }
+    );
   };
 
   const handleAddUser = async () => {
@@ -97,25 +108,46 @@ function App() {
       return;
     }
 
-    try {
-      const newUser = {
-        name: newName,
-        age: newAge,
-      };
-      await fetch('https://backend-j7qq.onrender.com/v1/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newUser),
-      });
-      setJsonData((prevData) => [...prevData, newUser]);
-      setNewName('');
-      setNewAge('');
-    } catch (error) {
-      console.error('Error adding user:', error);
-    }
+    const newUser = {
+      name: newName,
+      age: newAge,
+    };
+
+    handleFetch(
+      'https://backend-j7qq.onrender.com/v1/api/users',
+      'POST',
+      newUser,
+      (data) => {
+        setJsonData((prevData) => [...prevData, newUser]);
+        setNewName('');
+        setNewAge('');
+      },
+      (error) => {
+        console.error('Error adding user:', error);
+      }
+    );
   };
+
+  const renderFileInputs = (placeholder, onClickHandler) => (
+    <>
+      <input
+        type="text"
+        name="fileName"
+        value={fileName}
+        onChange={(e) => setFileName(e.target.value)}
+        placeholder="Enter file name"
+      />
+      <textarea
+        name="fileContent"
+        value={fileContent}
+        onChange={(e) => setFileContent(e.target.value)}
+        placeholder={placeholder}
+      />
+      <button className="app-button" onClick={onClickHandler}>
+        {placeholder}
+      </button>
+    </>
+  );
 
   return (
     <div className="app-container">
@@ -123,22 +155,7 @@ function App() {
 
       <div className="app-section">
         <h2>Create File</h2>
-        <input
-          type="text"
-          name="fileName"
-          value={fileName}
-          onChange={(e) => setFileName(e.target.value)}
-          placeholder="Enter file name"
-        />
-        <textarea
-          name="fileContent"
-          value={fileContent}
-          onChange={(e) => setFileContent(e.target.value)}
-          placeholder="Enter file content"
-        />
-        <button className="app-button" onClick={handleCreateFile}>
-          Create File
-        </button>
+        {renderFileInputs('Enter file content', handleCreateFile)}
       </div>
 
       <div className="app-section">
