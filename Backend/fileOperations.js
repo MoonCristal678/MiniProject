f5027767c4fdafa003700cb37f87ba1f190148f8
Krystal <n01554329@humber.ca>
2023-11-30 20:17:32 -0500

    push

--- Backend/fileOperations.js
+++ Backend/fileOperations.js
@@ -1,15 +1,17 @@
+// fileOperations.js
+
 import express from 'express';
 import methodOverride from 'method-override';
 import mongoose from 'mongoose';
 import { body, validationResult } from 'express-validator';
 import cors from 'cors';
-import File from './fileSchema.js'; // Import the file schema
+import File from './fileSchema.js';
 
 const v1Router = express.Router();
-
-
 const app = express();
 const port = 3000;
+
+// Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,114 +25,127 @@ mongoose.connect("mongodb+srv://blackkrystal438:DemonSlayer1@fileanduserdata.3yn
 });
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  bloodType: {
-    type: String,
-    required: true,
-  },
-  birthdate: {
-    type: Date,
-    required: true,
-  },
-  countryOfBirth: {
-    type: String,
-    required: true,
-  },
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  bloodType: { type: String, required: true },
+  birthdate: { type: Date, required: true },
+  countryOfBirth: { type: String, required: true },
 });
 
-// Create a Mongoose model for users
 const User = mongoose.model('User', userSchema);
 
+// Middleware for logging requests
 app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
-// Render home page with links to file and user operations
+// Home page route
 app.get('/', (req, res) => {
   res.send(`
     <h1>File Functionality</h1>
-    <button ><a href="/v1/read"> Read a File </a> </button> 
-    <button ><a href="/v1/write"> Write to a File </a> </button>
-    <button ><a href="/v1/delete"> Delete a File </a> </button>
-    <button ><a href="/v1/updateFile"> Update a File </a> </button>
-    <button ><a href="/v1/files"> View Files </a> </button>
+    <button><a href="/v1/read"> Read a File </a></button>
+    <button><a href="/v1/write"> Write to a File </a></button>
+    <button><a href="/v1/delete"> Delete a File </a></button>
+    <button><a href="/v1/updateFile"> Update a File </a></button>
+    <button><a href="/v1/files"> View Files </a></button>
     <br>
     <h1>User Functionality</h1>
-    <button ><a href="/v1/api/users"> Display JSON Data </a> </button>
-    <button ><a href="/v1/add"> Add User </a> </button>
-    <button ><a href="/v1/updateUser"> Update User </a> </button>
-    <button ><a href="/v1/deleteUser"> Delete User </a> </button>
-   
+    <button><a href="/v1/api/users"> Display JSON Data </a></button>
+    <button><a href="/v1/add"> Add User </a></button>
+    <button><a href="/v1/updateUser"> Update User </a></button>
+    <button><a href="/v1/deleteUser"> Delete User </a></button>
   `);
 });
 
-// Render form to add a new user
-v1Router.get('/add', (req, res) => {
-  res.render('addUser.ejs');
+// User routes
+v1Router.get('/add', renderAddUserForm);
+v1Router.post('/api/users', validateUserInput, addUser);
+v1Router.get('/api/users', getAllUsers);
+
+// File routes
+v1Router.get('/read', renderReadFileForm);
+v1Router.post('/read', readFile);
+v1Router.get('/write', renderWriteFileForm);
+v1Router.post('/write', validateFileInput, writeFile);
+v1Router.get('/files', viewFiles);
+v1Router.get('/delete', renderDeleteFileForm);
+v1Router.post('/delete', deleteFile);
+v1Router.get('/updateUser', renderUpdateUserForm);
+v1Router.post('/updateUser', updateUser);
+v1Router.get('/deleteUser', renderDeleteUserForm);
+v1Router.post('/deleteUser', deleteUser);
+v1Router.get('/updateFile', renderUpdateFileForm);
+v1Router.post('/updateFile', updateFile);
+
+// API versioning
+app.use('/v1', v1Router);
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
-// Add a new user with validation
-v1Router.post(
-  '/api/users',
-  [
+// Server start
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+// Helper functions
+
+function renderAddUserForm(req, res) {
+  res.render('addUser.ejs');
+}
+
+function validateUserInput(req, res, next) {
+  const validationRules = [
     body('name').notEmpty().trim().escape(),
     body('age').isInt({ min: 0 }),
     body('bloodType').notEmpty().trim().escape(),
     body('birthdate').isISO8601().toDate(),
     body('countryOfBirth').notEmpty().trim().escape(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
+  ];
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  validationResult(req);
+  next();
+}
 
-    const { name, age, bloodType, birthdate, countryOfBirth } = req.body;
+async function addUser(req, res) {
+  const errors = validationResult(req);
 
-    const newUser = new User({
-      name,
-      age,
-      bloodType,
-      birthdate,
-      countryOfBirth,
-    });
-
-    try {
-      await newUser.save();
-      res.json({ message: 'User added successfully', user: newUser });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
 
-// Get all users
-v1Router.get('/api/users', async (req, res) => {
-  console.log('Backend responded');
-  const users = await User.find({});
-  res.json(users);
-});
+  const { name, age, bloodType, birthdate, countryOfBirth } = req.body;
+  const newUser = new User({ name, age, bloodType, birthdate, countryOfBirth });
 
-// Render form to read a file
-v1Router.get('/read', async (req, res) => {
+  try {
+    await newUser.save();
+    res.json({ message: 'User added successfully', user: newUser });
+  } catch (error) {
+    handleServerError(res, error);
+  }
+}
+
+async function getAllUsers(req, res) {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    handleServerError(res, error);
+  }
+}
+
+async function renderReadFileForm(req, res) {
   const collection = mongoose.connection.db.collection('files');
   const files = await collection.find({}, { projection: { _id: 0, name: 1 } }).toArray();
   const fileNames = files.map(file => file.name);
   res.render('readFile.ejs', { fileNames });
-});
+}
 
-// Using async/await for read route
-v1Router.post('/read', async (req, res) => {
+async function readFile(req, res) {
   const fileName = req.body.fileName;
 
   const collection = mongoose.connection.db.collection('files');
@@ -147,9 +162,9 @@ v1Router.post('/read', async (req, res) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-});
+}
 
-v1Router.get('/write', async (req, res) => {
+async function renderWriteFileForm(req, res) {
   try {
     const files = await File.find({});
     res.render('writeFile.ejs', { files });
@@ -157,60 +172,55 @@ v1Router.get('/write', async (req, res) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-});
+}
 
-// Add a new file with validation
-v1Router.post(
-  '/write',
-  [
+function validateFileInput(req, res, next) {
+  const validationRules = [
     body('fileName').notEmpty().trim().escape(),
     body('fileContent').notEmpty().trim().escape(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
+  ];
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  validationResult(req);
+  next();
+}
 
-    const { fileName, fileContent } = req.body;
+async function writeFile(req, res) {
+  const errors = validationResult(req);
 
-    const newFile = new File({
-      name: fileName,
-      content: fileContent,
-    });
-
-    try {
-      await newFile.save();
-      res.json({ message: 'File created successfully', file: newFile });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
-v1Router.get('/files', async (req, res) => {
+
+  const { fileName, fileContent } = req.body;
+  const newFile = new File({ name: fileName, content: fileContent });
+
+  try {
+    await newFile.save();
+    res.json({ message: 'File created successfully', file: newFile });
+  } catch (error) {
+    handleServerError(res, error);
+  }
+}
+
+async function viewFiles(req, res) {
   try {
     const files = await File.find({});
     res.json(files);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
-// Render form to delete a file
-v1Router.get('/delete', async (req, res) => {
+}
+
+async function renderDeleteFileForm(req, res) {
   const collection = mongoose.connection.db.collection('files');
   const files = await collection.find({}, { projection: { _id: 0, name: 1 } }).toArray();
   const fileNames = files.map(file => file.name);
   res.render('deleteFile.ejs', { fileNames });
-});
+}
 
-// Using async/await for delete route
-v1Router.post('/delete', async (req, res) => {
+async function deleteFile(req, res) {
   const { fileName } = req.body;
   console.log('Deleting file on the server:', fileName);
-
 
   const collection = mongoose.connection.db.collection('files');
   const result = await collection.deleteOne({ name: fileName });
@@ -220,27 +230,24 @@ v1Router.post('/delete', async (req, res) => {
   } else {
     res.status(404).send('File not found.');
   }
-});
+}
 
-// Render form to update a user
-v1Router.get('/updateUser', async (req, res) => {
+async function renderUpdateUserForm(req, res) {
   try {
     const users = await User.find({});
     res.render('updateUser.ejs', { users });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
+}
 
-// Using async/await for update route
-v1Router.post('/updateUser', async (req, res) => {
-  const userId = req.body.userId; 
+async function updateUser(req, res) {
+  const userId = req.body.userId;
+
   try {
     const user = await User.findById(userId);
 
     if (user) {
-      // Update the user fields as needed
       user.name = req.body.name;
       user.age = req.body.age;
       user.bloodType = req.body.bloodType;
@@ -253,25 +260,22 @@ v1Router.post('/updateUser', async (req, res) => {
       res.status(404).json({ message: 'User not found' });
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
+}
 
-// Render form to delete a user
-v1Router.get('/deleteUser', async (req, res) => {
+async function renderDeleteUserForm(req, res) {
   try {
     const users = await User.find({});
     res.render('deleteUser.ejs', { users });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
+}
 
-// Using async/await for delete route
-v1Router.post('/deleteUser', async (req, res) => {
-  const userId = req.body.userId; 
+async function deleteUser(req, res) {
+  const userId = req.body.userId;
+
   try {
     const result = await User.deleteOne({ _id: userId });
 
@@ -281,30 +285,26 @@ v1Router.post('/deleteUser', async (req, res) => {
       res.status(404).json({ message: 'User not found' });
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
+}
 
-
-v1Router.get('/updateFile', async (req, res) => {
+async function renderUpdateFileForm(req, res) {
   try {
     const files = await File.find({});
     res.render('updateFile.ejs', { files });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
+}
 
-// Using async/await for update file route
-v1Router.post('/updateFile', async (req, res) => {
+async function updateFile(req, res) {
   const fileId = req.body.fileId;
+
   try {
     const file = await File.findById(fileId);
 
     if (file) {
-      // Update the file fields as needed
       file.name = req.body.name;
       file.content = req.body.content;
 
@@ -314,21 +314,11 @@ v1Router.post('/updateFile', async (req, res) => {
       res.status(404).json({ message: 'File not found' });
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    handleServerError(res, error);
   }
-});
+}
 
-// Use API versioning
-app.use('/v1', v1Router);
-
-// Define error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+function handleServerError(res, error) {
+  console.error(error);
+  res.status(500).send('Internal Server Error');
+}
