b3540436b0a3982717e5aad32de0241bd1d06b7b
Krystal <n01554329@humber.ca>
2023-12-07 11:13:49 -0500

    push

--- Backend/fileOperations.js
+++ Backend/fileOperations.js
@@ -27,6 +27,13 @@ app.use(session({
   },
   credentials: true,
 }));
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
